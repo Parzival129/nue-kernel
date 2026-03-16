c0d663403f0d08121b4812bb59bc429ee5a874e9
Parzival129 <beacheshiro@gmail.com>
2026-03-16 16:46:11 -0400

    Added syscalls stubs for newlib c library

--- kernel/arch/i386/idt.c
+++ kernel/arch/i386/idt.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 struct idt_entry idt[NO_IDT_ENTRIES]; // 256 descriptors to fulfill i386 arch.
-struct idt_ptr ip;                    // pointer to idt
+struct idt_ptr ip; // pointer to idt
 
 extern void idt_init(uint32_t);
 
@@ -14,70 +14,70 @@ uint8_t cli_buffer_index = 0;
 // Exception and interrupt handler stubs, based off i386 standards
 // Implemented in isr.s
 
-extern void isr0(void);   // Division by Zero
-extern void isr1(void);   // Debug
-extern void isr2(void);   // Non-Maskable Interrupt
-extern void isr3(void);   // Breakpoint
-extern void isr4(void);   // Into Detected Overflow
-extern void isr5(void);   // Bound Range Exceeded
-extern void isr6(void);   // Invalid Opcode
-extern void isr7(void);   // Coprocessor not available
-extern void isr8(void);   // Double Fault
-extern void isr9(void);   // Coprocessor Segment Overrun
-extern void isr10(void);  // Invalid TSS
-extern void isr11(void);  // Segment Not Present
-extern void isr12(void);  // Stack-Segment Fault
-extern void isr13(void);  // General Protection Fault
-extern void isr14(void);  // Page Fault
-extern void isr15(void);  // Reserved
-extern void isr16(void);  // Floating Point Exception
-extern void isr17(void);  // Alignment Check
-extern void isr18(void);  // Machine Check
-extern void isr19(void);  // SIMD Floating-Point Exception
-extern void isr20(void);  // Virtualization Exception
-extern void isr21(void);  // Control Protection Exception
-extern void isr22(void);  // Reserved by CPU
-extern void isr23(void);  // Reserved
-extern void isr24(void);  // Reserved
-extern void isr25(void);  // Reserved
-extern void isr26(void);  // Reserved
-extern void isr27(void);  // Reserved
-extern void isr28(void);  // Reserved
-extern void isr29(void);  // Reserved
-extern void isr30(void);  // Security Exception
-extern void isr31(void);  // Reserved
+extern void isr0(void); // Division by Zero
+extern void isr1(void); // Debug
+extern void isr2(void); // Non-Maskable Interrupt
+extern void isr3(void); // Breakpoint
+extern void isr4(void); // Into Detected Overflow
+extern void isr5(void); // Bound Range Exceeded
+extern void isr6(void); // Invalid Opcode
+extern void isr7(void); // Coprocessor not available
+extern void isr8(void); // Double Fault
+extern void isr9(void); // Coprocessor Segment Overrun
+extern void isr10(void); // Invalid TSS
+extern void isr11(void); // Segment Not Present
+extern void isr12(void); // Stack-Segment Fault
+extern void isr13(void); // General Protection Fault
+extern void isr14(void); // Page Fault
+extern void isr15(void); // Reserved
+extern void isr16(void); // Floating Point Exception
+extern void isr17(void); // Alignment Check
+extern void isr18(void); // Machine Check
+extern void isr19(void); // SIMD Floating-Point Exception
+extern void isr20(void); // Virtualization Exception
+extern void isr21(void); // Control Protection Exception
+extern void isr22(void); // Reserved by CPU
+extern void isr23(void); // Reserved
+extern void isr24(void); // Reserved
+extern void isr25(void); // Reserved
+extern void isr26(void); // Reserved
+extern void isr27(void); // Reserved
+extern void isr28(void); // Reserved
+extern void isr29(void); // Reserved
+extern void isr30(void); // Security Exception
+extern void isr31(void); // Reserved
 
 // IRQ handlers for hardware interrupts: signal sent by hardware to cpu "I have data for you"
-extern void irq0(void);   // IRQ0 - Timer
-extern void irq1(void);   // IRQ1 - Keyboard
-extern void irq2(void);   // IRQ2 - Cascade
-extern void irq3(void);   // IRQ3 - COM2
-extern void irq4(void);   // IRQ4 - COM1
-extern void irq5(void);   // IRQ5 - LPT2
-extern void irq6(void);   // IRQ6 - Floppy
-extern void irq7(void);   // IRQ7 - LPT1
-extern void irq8(void);   // IRQ8 - Real Time Clock
-extern void irq9(void);   // IRQ9 - Peripheral
-extern void irq10(void);  // IRQ10 - Peripheral
-extern void irq11(void);  // IRQ11 - Peripheral
-extern void irq12(void);  // IRQ12 - Mouse
-extern void irq13(void);  // IRQ13 - Coprocessor
-extern void irq14(void);  // IRQ14 - ATA Primary
-extern void irq15(void);  // IRQ15 - ATA Secondary
+extern void irq0(void); // IRQ0 - Timer
+extern void irq1(void); // IRQ1 - Keyboard
+extern void irq2(void); // IRQ2 - Cascade
+extern void irq3(void); // IRQ3 - COM2
+extern void irq4(void); // IRQ4 - COM1
+extern void irq5(void); // IRQ5 - LPT2
+extern void irq6(void); // IRQ6 - Floppy
+extern void irq7(void); // IRQ7 - LPT1
+extern void irq8(void); // IRQ8 - Real Time Clock
+extern void irq9(void); // IRQ9 - Peripheral
+extern void irq10(void); // IRQ10 - Peripheral
+extern void irq11(void); // IRQ11 - Peripheral
+extern void irq12(void); // IRQ12 - Mouse
+extern void irq13(void); // IRQ13 - Coprocessor
+extern void irq14(void); // IRQ14 - ATA Primary
+extern void irq15(void); // IRQ15 - ATA Secondary
 
 // Functions for direct hardware communication:
 
 // writes one byte of data to a specific hardware I/O port
 void outb(uint16_t port, uint8_t value)
 {
-    __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+    __asm__ volatile("outb %0, %1" : : "a"(value), "Nd"(port));
 }
 
-// read one byte of data from a specific hardware I/O port 
+// read one byte of data from a specific hardware I/O port
 uint8_t inb(uint16_t port)
 {
     uint8_t value;
-    __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
+    __asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port));
     return value;
 }
 
@@ -86,7 +86,6 @@ static inline void io_wait(void)
 {
     outb(0x80, 0); // fake write that takes about 1 microsecond
 }
-
 
 // Remaps the PIC to move hardware interrupts from their conflicting default range (0x08–0x0F) to a safe range (usually 0x20+)
 // prevents hardware IRQs from being misinterpreted by the CPU as internal exceptions or faults, normally not a problem in real mode but will interfere in protected mode (how the kernel runs)
@@ -130,12 +129,12 @@ static void pic_send_eoi(uint8_t irq)
     outb(0x20, 0x20);
 }
 
-void isr_handler(struct interrupt_frame *frame) // handles the interrupt service routines passed back from the stubs
+void isr_handler(struct interrupt_frame* frame) // handles the interrupt service routines passed back from the stubs
 // Uses two-stage assembly wrapping method (stubs defined in assembly file and handler function in C)
 {
     if (frame->int_no < 32) { // the interrupt is for a execption
         // maps each isr to a exception message, matches idt defined below
-        static const char *exception_messages[32] = {
+        static const char* exception_messages[32] = {
             "Division By Zero",
             "Debug",
             "Non-Maskable Interrupt",
@@ -171,12 +170,12 @@ void isr_handler(struct interrupt_frame *frame) // handles the interrupt service
         };
 
         printf("[EXCEPTION] #%u: %s (err=0x%x)\n",
-               frame->int_no,
-               exception_messages[frame->int_no],
-               frame->err_code);
+            frame->int_no,
+            exception_messages[frame->int_no],
+            frame->err_code);
 
         for (;;) {
-            __asm__ volatile ("cli; hlt"); // halts everything since exceptions are unrecoverable at this stage
+            __asm__ volatile("cli; hlt"); // halts everything since exceptions are unrecoverable at this stage
         }
     }
 
@@ -190,18 +189,18 @@ void isr_handler(struct interrupt_frame *frame) // handles the interrupt service
             if (c != 0) {
                 if (c == '\n') { // when the user presses enter key
                     putchar(c); // write the newline to the screen
-                    cli_buffer[cli_buffer_index] = '\0';
+                    cli_buffer[cli_buffer_index] = '\0'; // terminate cli command buffer
 
                     // Refactor this into seperate command router
                     if (cli_buffer_index == 4 && memcmp(cli_buffer, "info", 4) == 0) {
                         printf("Nue Kernel v0.1\n");
-                    } else if (cli_buffer[0] != '\0') {
+                    } else if (cli_buffer[0] != '\0') { // if the command buffer is not empty, then throw an error
                         printf("command '%s' not recognized\n", cli_buffer);
                     }
 
-                    cli_buffer_index = 0;
+                    cli_buffer_index = 0; // reset after enter pressed
                     cli_buffer[0] = '\0';
-                    printf(">");
+                    printf(">"); // new prompt
                 } else if (c == '\b') {
                     if (cli_buffer_index > 0) {
                         cli_buffer_index--;
@@ -241,16 +240,16 @@ void idt_install()
     // Set up CPU exceptions (0-31)
     // Includes index of IDT entry, address of interrupt handler code, the segment selector, and attributes
     // Reference 0x08 kernel code segment that was defined in the GDT -> knows what code segment to run the handler in
-    idt_set_entry(0, (uint32_t)isr0, 0x08, 0x8E);   // Division by Zero Exception
-    idt_set_entry(1, (uint32_t)isr1, 0x08, 0x8E);   // Debug Exception
-    idt_set_entry(2, (uint32_t)isr2, 0x08, 0x8E);   // NMI Exception
-    idt_set_entry(3, (uint32_t)isr3, 0x08, 0x8E);   // Breakpoint Exception
-    idt_set_entry(4, (uint32_t)isr4, 0x08, 0x8E);   // Into Detected Overflow Exception
-    idt_set_entry(5, (uint32_t)isr5, 0x08, 0x8E);   // Bound Range Exceeded Exception
-    idt_set_entry(6, (uint32_t)isr6, 0x08, 0x8E);   // Invalid Opcode Exception
-    idt_set_entry(7, (uint32_t)isr7, 0x08, 0x8E);   // Coprocessor Not Available Exception
-    idt_set_entry(8, (uint32_t)isr8, 0x08, 0x8E);   // Double Fault Exception
-    idt_set_entry(9, (uint32_t)isr9, 0x08, 0x8E);   // Coprocessor Segment Overrun Exception
+    idt_set_entry(0, (uint32_t)isr0, 0x08, 0x8E); // Division by Zero Exception
+    idt_set_entry(1, (uint32_t)isr1, 0x08, 0x8E); // Debug Exception
+    idt_set_entry(2, (uint32_t)isr2, 0x08, 0x8E); // NMI Exception
+    idt_set_entry(3, (uint32_t)isr3, 0x08, 0x8E); // Breakpoint Exception
+    idt_set_entry(4, (uint32_t)isr4, 0x08, 0x8E); // Into Detected Overflow Exception
+    idt_set_entry(5, (uint32_t)isr5, 0x08, 0x8E); // Bound Range Exceeded Exception
+    idt_set_entry(6, (uint32_t)isr6, 0x08, 0x8E); // Invalid Opcode Exception
+    idt_set_entry(7, (uint32_t)isr7, 0x08, 0x8E); // Coprocessor Not Available Exception
+    idt_set_entry(8, (uint32_t)isr8, 0x08, 0x8E); // Double Fault Exception
+    idt_set_entry(9, (uint32_t)isr9, 0x08, 0x8E); // Coprocessor Segment Overrun Exception
     idt_set_entry(10, (uint32_t)isr10, 0x08, 0x8E); // Invalid TSS Exception
     idt_set_entry(11, (uint32_t)isr11, 0x08, 0x8E); // Segment Not Present Exception
     idt_set_entry(12, (uint32_t)isr12, 0x08, 0x8E); // Stack-Segment Fault Exception
@@ -275,16 +274,16 @@ void idt_install()
     idt_set_entry(31, (uint32_t)isr31, 0x08, 0x8E); // Reserved Exception
 
     // Set up IRQ (Interrupt request) handlers (for hardware) (mapped to IDT 32-47 after PIC remapping)
-    idt_set_entry(32, (uint32_t)irq0, 0x08, 0x8E);  // IRQ0 - Timer
-    idt_set_entry(33, (uint32_t)irq1, 0x08, 0x8E);  // IRQ1 - Keyboard
-    idt_set_entry(34, (uint32_t)irq2, 0x08, 0x8E);  // IRQ2 - Cascade
-    idt_set_entry(35, (uint32_t)irq3, 0x08, 0x8E);  // IRQ3 - COM2
-    idt_set_entry(36, (uint32_t)irq4, 0x08, 0x8E);  // IRQ4 - COM1
-    idt_set_entry(37, (uint32_t)irq5, 0x08, 0x8E);  // IRQ5 - LPT2
-    idt_set_entry(38, (uint32_t)irq6, 0x08, 0x8E);  // IRQ6 - Floppy
-    idt_set_entry(39, (uint32_t)irq7, 0x08, 0x8E);  // IRQ7 - LPT1
-    idt_set_entry(40, (uint32_t)irq8, 0x08, 0x8E);  // IRQ8 - Real Time Clock
-    idt_set_entry(41, (uint32_t)irq9, 0x08, 0x8E);  // IRQ9 - Peripheral
+    idt_set_entry(32, (uint32_t)irq0, 0x08, 0x8E); // IRQ0 - Timer
+    idt_set_entry(33, (uint32_t)irq1, 0x08, 0x8E); // IRQ1 - Keyboard
+    idt_set_entry(34, (uint32_t)irq2, 0x08, 0x8E); // IRQ2 - Cascade
+    idt_set_entry(35, (uint32_t)irq3, 0x08, 0x8E); // IRQ3 - COM2
+    idt_set_entry(36, (uint32_t)irq4, 0x08, 0x8E); // IRQ4 - COM1
+    idt_set_entry(37, (uint32_t)irq5, 0x08, 0x8E); // IRQ5 - LPT2
+    idt_set_entry(38, (uint32_t)irq6, 0x08, 0x8E); // IRQ6 - Floppy
+    idt_set_entry(39, (uint32_t)irq7, 0x08, 0x8E); // IRQ7 - LPT1
+    idt_set_entry(40, (uint32_t)irq8, 0x08, 0x8E); // IRQ8 - Real Time Clock
+    idt_set_entry(41, (uint32_t)irq9, 0x08, 0x8E); // IRQ9 - Peripheral
     idt_set_entry(42, (uint32_t)irq10, 0x08, 0x8E); // IRQ10 - Peripheral
     idt_set_entry(43, (uint32_t)irq11, 0x08, 0x8E); // IRQ11 - Peripheral
     idt_set_entry(44, (uint32_t)irq12, 0x08, 0x8E); // IRQ12 - Mouse
@@ -300,5 +299,5 @@ void idt_install()
     idt_init((uint32_t)&ip);
 
     // Enable hardware interrupts after IDT + PIC are initialized.
-    __asm__ volatile ("sti"); // CPU will now respond to interrupts, previously they were ignored before setup was complete
+    __asm__ volatile("sti"); // CPU will now respond to interrupts, previously they were ignored before setup was complete
 }
---
+++ kernel/arch/i386/syscalls.c
@@ -0,0 +1,81 @@
+// Implement POSIX-like stubs for the 3rd party C library to use
+
+#include <stddef.h> // provides size_t and NULL
+#include <stdint.h> // intptr_t and uint8_t
+// #include <sys/stat.h> // gives structs
+#include <kernel/tty.h> // get terminal_write
+#include <string.h>
+
+int errno; // will be intialized to 0 since its in BSS, set to associated error numbers when necessary
+extern uint8_t _kernel_end[]; // for compiler to dereference where kernel ends
+static uint8_t* heap_ptr = _kernel_end; // define where the heap begins
+
+#define HEAP_MAX 0x100000 // 1 MiB
+#define EBADF 9
+#define S_IFCHR 0020000
+
+struct stat {
+    int st_mode;
+};
+
+void* _sbrk(intptr_t increment) // move heap pointer
+{
+    uint8_t* prev = heap_ptr; // assign the current heap pointer to a temporary prev variable
+    if (heap_ptr + increment > _kernel_end + HEAP_MAX) { // remember to start at _kernel_end memory address
+        return (void*)-1; // out of heap space
+    }
+    heap_ptr += increment; // move the heap pointer
+    return (void*)prev; // return the previous position as the start of the new block
+}
+
+int _write(int fd, const char* buf, int len)
+{ // write to the terminal
+    if (fd == 1 || fd == 2) {
+        terminal_write(buf, (size_t)len);
+        return len;
+    }
+    errno = EBADF; // bad file descriptor error, "I don't know what file you're talking about"
+    return -1; // error
+}
+
+int _read(int fd, char* buf, int len)
+{
+    (void)fd; (void)buf; (void)len; // void unused args
+    return -1; // not necessary at the moment since idt writes directly to the screen buffer, not file descriptor
+}
+
+// completely stops the kernel and halts the cpu indefinitely
+__attribute__((noreturn)) void _exit(int status)
+{ // signal that this function has no return val since it is the exit function
+    (void)status;
+    asm volatile ("cli; 1: hlt; jmp 1b");
+    __builtin_unreachable();
+}
+
+int _close(int fd)
+{ // close a file descriptor
+    (void)fd;
+    return -1; // has nothing to close at the moment
+}
+
+int _fstat(int fd, struct stat* st)
+{
+    (void)fd;
+    memset(st, 0, sizeof(struct stat));
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _isatty(int fd)
+{ // checks if a file descriptor is tty
+    if (fd == 0 || fd == 1 || fd == 2) {
+        return 1;
+    }
+    return 0;
+}
+
+int _lseek(int fd, int offset, int whence)
+{ // can't seek on a character device like i386
+    (void)fd; (void)offset; (void)whence;
+    return -1;
+}
