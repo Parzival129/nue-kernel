5993cee20438dcecde3f2844c2ab3f4e3061dd21
Parzival129 <beacheshiro@gmail.com>
2026-03-18 13:56:01 -0400

    Added todo list to readme.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # nue kernel
 
 This is a homebrew kernel that I am buliding based off the foundation outlined in the OSDev.org materials. I am building this to gain a deeper undersanding of C and low level programming as a whole. 
+
+To be implemented:
+
+- X meat kernel from OSDev
+- X gdt / idt
+- X pic controller
+- X ps2 keyboard input
+- X ported newlib C library
+- paging / virtual mem.
+- physical memory manager
+- heap allocator
+- timer / RTC
+- filesystem (read-only)
+- ring 3 usermode
+- port cool software (C compiler? text editor)
+- gui graphics?
+- basic networking?
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
-#include <kernel/tty.h>
 #include <kernel/gdt.h>
 #include <kernel/idt.h>
+#include <kernel/tty.h>
 
-void kernel_main(void) {
-	terminal_initialize();
-	setvbuf(stdout, NULL, _IONBF, 0); // disable stdout buffering so putchar writes immediately
-	printf("[OK] terminal initialized\n");
-	gdt_install(); printf("[OK] gdt installed\n");
-	idt_install(); printf("[OK] idt installed\n");
+void kernel_main(void)
+{
+    terminal_initialize();
+    setvbuf(stdout, NULL, _IONBF, 0); // disable stdout buffering so putchar writes immediately
+    printf("[OK] terminal initialized\n");
+    gdt_install();
+    printf("[OK] gdt installed\n");
+    idt_install();
+    printf("[OK] idt installed\n");
 
-	printf("                   __                    __\n");
-	printf("  ___  __ _____   / /_____ _______  ___ / /\n");
-	printf(" / _ \\/ // / -_) /  '_/ -_) __/ _ \\/ -_) / \n");
-	printf("/_//_/\\_,_/\\__/ /_/\\_\\\\__/_/ /_//_/\\__/_/  \n"); 
-                                           
-	printf("booted\n\n");
-	printf("READY\n>");
+    printf("                   __                    __\n");
+    printf("  ___  __ _____   / /_____ _______  ___ / /\n");
+    printf(" / _ \\/ // / -_) /  '_/ -_) __/ _ \\/ -_) / \n");
+    printf("/_//_/\\_,_/\\__/ /_/\\_\\\\__/_/ /_//_/\\__/_/  \n");
 
-	// Tests the IDT exception handler with a division by 0 exception
-	// __asm__ volatile ("movl $1, %%eax; xorl %%edx, %%edx; movl $0, %%ecx; divl %%ecx" ::: "eax", "ecx", "edx");
-	
-	for (;;) { // halts the program so it can observe IRQ interrupts for keyboard testing
-		__asm__ volatile ("hlt");
-	}
+    printf("booted\n\n");
+    printf("READY\n>");
+
+    // Tests the IDT exception handler with a division by 0 exception
+    // __asm__ volatile ("movl $1, %%eax; xorl %%edx, %%edx; movl $0, %%ecx; divl %%ecx" ::: "eax", "ecx", "edx");
+
+    for (;;) { // halts the program so it can observe IRQ interrupts for keyboard testing
+        __asm__ volatile("hlt");
+    }
 }
