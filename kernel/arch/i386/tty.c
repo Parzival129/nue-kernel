2e4ed05bb8cb5ad88c7ff23953443f58c4c693ab
Parzival129 <beacheshiro@gmail.com>
2026-04-19 23:59:25 -0400

    Fixed semicolon error.

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -163,7 +163,6 @@ void terminal_putchar(char c)
 }
 
 void terminal_clear() {
-    terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
     terminal_buffer = VGA_MEMORY;
     for (size_t y = 0; y < VGA_HEIGHT; y++) {
         for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -171,7 +170,7 @@ void terminal_clear() {
             terminal_buffer[index] = vga_entry(' ', terminal_color);
         }
     }
-    update_cursor(0,0)
+    update_cursor(0,0);
 }
 
 void terminal_write(const char* data, size_t size)
