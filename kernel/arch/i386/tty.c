db13c2a183bfd3a78a256c38fc2ec59dd3673607
Parzival129 <beacheshiro@gmail.com>
2026-04-19 23:58:44 -0400

    Created tty function to clear terminal properly.

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -162,6 +162,18 @@ void terminal_putchar(char c)
     }
 }
 
+void terminal_clear() {
+    terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+    terminal_buffer = VGA_MEMORY;
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(' ', terminal_color);
+        }
+    }
+    update_cursor(0,0)
+}
+
 void terminal_write(const char* data, size_t size)
 {
     for (size_t i = 0; i < size; i++) {
